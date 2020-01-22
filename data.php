b7f2dc27de4449be874d1051a90cc5721f130065
WolfHugo <1031629044@qq.com>
2020-01-22 23:57:24 +0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,40 @@
+<?php
+
+// $data_array = array(
+//     array(
+//         'a', array(
+//             array(
+//                 'aa', 'bb', 'cc'
+//             ),
+//             array(
+//                 'aa', 'bb', 'cc'
+//             ),
+//             array(
+//                 'aa', 'bb', 'cc'
+//             )
+//         )
+//     ),
+//     array(
+//         'a', array(
+//             array(
+//                 'aa', 'bb', 'cc'
+//             ),
+//             array(
+//                 'aa', 'bb', 'cc'
+//             ),
+//             array(
+//                 'aa', 'bb', 'cc'
+//             )
+//         )
+//     )
+// );
+// echo json_encode($data_array);
+
+if(isset($_POST['opt'])){
+    switch($_POST['opt']){
+        case 'add':{
+            
+
+        }
+    }
+}
